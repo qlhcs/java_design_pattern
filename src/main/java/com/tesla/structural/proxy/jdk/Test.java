5f0c66f84d2d0d429af47e5256e866ab1db644f3
longhaoqiu <longhaoqiucs@gmail.com>
2024-05-20 20:01:27 +0800

    opt

--- src/main/java/com/tesla/structural/proxy/jdk/Test.java
+++ src/main/java/com/tesla/structural/proxy/jdk/Test.java
@@ -16,10 +16,7 @@ public class Test {
         service.create();
     }
 
-    public static void testClass() throws Exception {
-        // ========== 添加了这行代码之后，可以将在项目目录下保存动态创建的class文件 ==========
-        System.getProperties().put("jdk.proxy.ProxyGenerator.saveGeneratedFiles", "true");
-
+    public static void getProxyInfo() {
         // ========== 代理类class相关信息 ==========
         Class<?> proxyClass = Proxy.getProxyClass(UserServiceImpl.class.getClassLoader(),
                 UserServiceImpl.class.getInterfaces());
@@ -30,13 +27,21 @@ public class Test {
         }
         // 代理类所有构造函数
         System.out.println(Arrays.toString(proxyClass.getConstructors()));
+    }
+
+    public static void testClass() throws Exception {
+        // ========== 添加了这行代码之后，可以将在项目目录下保存动态创建的class文件 ==========
+        System.getProperties().put("jdk.proxy.ProxyGenerator.saveGeneratedFiles", "true");
 
         // ========== 通过反射创建代理类对象 ==========
+        Class<?> proxyClass = Proxy.getProxyClass(UserServiceImpl.class.getClassLoader(),
+                UserServiceImpl.class.getInterfaces());
         // 获取$Proxy0(InvocationHandler handler)构造方法
         Constructor<?> constructor = proxyClass.getConstructor(InvocationHandler.class);
         UserService userService = (UserService) constructor.newInstance(new InvocationHandler() {
             @Override
             public Object invoke(Object proxy, Method method, Object[] args) {
+                // 调用增强方法时，打印代理类类型、调用方法名
                 System.out.println("===== enter enhanced class invoke method =====");
                 System.out.println(proxy.getClass());
                 System.out.println(method.getDeclaringClass() + "." + method.getName() + "()");
